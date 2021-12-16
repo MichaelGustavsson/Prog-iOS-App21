6cb1d37f63ad2178e40aedfdbb60367e898f98e3
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2021-12-16 14:14:30 +0100

    Day-3 updated code

--- dag-3/SimpleApp/ViewController.swift
+++ dag-3/SimpleApp/ViewController.swift
@@ -15,10 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImage: UIImageView!
     @IBOutlet weak var changeHeaderText: UIButton!
     
+    var imgIdx = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         
         headerLabel.text = "Min super APP!"
+        diceImage.image = #imageLiteral(resourceName: "dice-1")
     }
 
     @IBAction func changeTextButton(_ sender: UIButton) {
@@ -26,8 +28,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func changeImageButton(_ sender: UIButton) {
-        diceImage.image = #imageLiteral(resourceName: "dice-5")
-        changeHeaderText.setTitle("Nytt namn", for: .normal)
+        let images = [ #imageLiteral(resourceName: "dice-1"), #imageLiteral(resourceName: "dice-2"), #imageLiteral(resourceName: "dice-3"), #imageLiteral(resourceName: "dice-4"), #imageLiteral(resourceName: "dice-5"), #imageLiteral(resourceName: "dice-6")]
+        var array = [1,2,3,4,5,6,7,8,9]
+        
+        print(array)
+        
+        array.shuffle()
+        print(array)
+        
+        array.sort()
+        print(array)
+        
+        diceImage.image = images.randomElement()
+//        print(Int.random(in: 1...20))
+        
+//        diceImage.image = images[Int.random(in: 0..<images.count)]
+        
+//        diceImage.image = [ #imageLiteral(resourceName: "dice-1"), #imageLiteral(resourceName: "dice-2"), #imageLiteral(resourceName: "dice-3"), #imageLiteral(resourceName: "dice-4"), #imageLiteral(resourceName: "dice-5"), #imageLiteral(resourceName: "dice-6")][Int.random(in: 0...5)]
+//
+//        imgIdx += 1
+//        changeHeaderText.setTitle("Nytt namn", for: .normal)
     }
 }
 
