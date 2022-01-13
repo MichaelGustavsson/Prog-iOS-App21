10eeeb4920b55bb48d0a0eb814434082b690b074
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-01-13 10:53:24 +0100

    Update Westcoast Cars table view with fancyCell

--- Dag-7/WestCoast-Cars/WestCoast-Cars/VehicleMakeTableViewController.swift
+++ Dag-7/WestCoast-Cars/WestCoast-Cars/VehicleMakeTableViewController.swift
@@ -41,9 +41,9 @@ class VehicleMakeTableViewController: UITableViewController {
     
     // Steg 1. Konfigurera datakällan till att använda UITableViewDiffableDataSource...
     func setupDataSource() -> UITableViewDiffableDataSource<Section, String>{
-        let cellIdentifier = "makeBaseCell"
+        //let cellIdentifier = "makeBaseCell"
         //let cellIdentifier = "makeFancyCell"
-        
+        let cellIdentifier = "makeNiceCell"
         // Skapa en instans av UITableViewDiffableDatasource...
         let dataSource = UITableViewDiffableDataSource<Section, String>(tableView: tableView,
                                                                         cellProvider: {
@@ -72,73 +72,6 @@ class VehicleMakeTableViewController: UITableViewController {
         }
     }
 
-//    //Hur många sektioner i vår tabell behöver vi?
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return vehicles.count
-//    }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cellIdentifier = "makeBaseCell"
-////        let cellIdentifier = "makeFancyCell"
-//        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MakeTableViewCell
-//
-//        //Konfigurera vår prototyp cell...
-//        cell.makeNameLabel.text = vehicles[indexPath.row]
-//        cell.availabilityLabel.text = "Antal bilar i lager 2"
-//        cell.thumbnailImage.image = UIImage(named: logos[indexPath.row])
-//
-//        return cell
-//    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        // Skapa en dialog...
-//        //Instantiera en instans av klassen UIAlertController...
-//        let menu = UIAlertController(title: "Vårt första ActionSheet", message: "Vad vill du göra?", preferredStyle: .actionSheet)
-//        //let menu = UIAlertController(title: nil, message: "Vad vill du göra?", preferredStyle: .alert)
-//
-//        //Skapa en händelse/Action
-//        let cancelAction = UIAlertAction(title: "Avbryt", style: .cancel, handler: nil)
-//
-//        //Skapa en closure metod (Completion handler / handler:)...
-//        let testActionHandler = {
-//            (action: UIAlertAction) -> Void in // Här kommer koden som skall köras
-//
-//            let message = UIAlertController(title: "Bekräfta", message: "Är du helt säker?", preferredStyle: .alert)
-//
-//            message.addAction(UIAlertAction(title: "Ta bort", style: .destructive, handler: nil))
-//            self.present(message, animated: true, completion: nil)
-//        }
-//
-//        let demoAction = UIAlertAction(title: "Ta bort", style: .destructive, handler: testActionHandler)
-//
-//        // Skapa en möjlighet att sätta en viss tillverkare som favorite...
-//        let favoriteAction = UIAlertAction(title: "Gör till favorit", style: .default, handler: {
-//            (action: UIAlertAction!) -> Void in
-//
-//            let cell = tableView.cellForRow(at: indexPath)
-//            cell?.accessoryType = .checkmark
-//
-//        })
-//
-//        menu.addAction(demoAction)
-//        menu.addAction(favoriteAction)
-//        menu.addAction(cancelAction)
-//
-//        //Presentera/visa dialogrutan.
-//        present(menu, animated: true, completion: nil)
-//
-//        //Avmarkera markerad rad...
-//        tableView.deselectRow(at: indexPath, animated: false)
-    }
-    
-    
-    
     //Göm topp menyn
     override var prefersStatusBarHidden: Bool{
         return true
