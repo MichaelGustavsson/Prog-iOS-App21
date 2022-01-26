9b38097fbc070bb2ad0e4a660222f7e84f7374cf
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-01-26 19:05:34 +0100

    Added day-12 and a starter folder with a start up project for our WeatherApp

---
+++ dag-12/readme.md
@@ -0,0 +1,4 @@
+# Kod för lektion dag-12
+### Här finns två kataloger en starter och en final
+starter innehåller bas kod för vår Weather app sedan sist. Den är komplett med anrop till openweathermap.org och dess REST API.
+Layouten är helt gjord med **constraints** och inte med **Stack Layout**
---
+++ dag-12/starter/WeatherApp/WeatherApp/Models/WeatherModel.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Michael Gustavsson on 2022-01-25.
+//
+
+import Foundation
+
+struct WeatherModel {
+    // Stored properties
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    let feelsLike: Double
+    let humidity: Int
+    
+    // Computed properties
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    var feelsLikeString: String {
+        return String(format: "%.1f", feelsLike)
+    }
+    
+    var conditionName: String {
+        print(conditionId)
+        switch conditionId {
+            case 200...232:
+            return "cloud.bolt"
+            case 300...321:
+                return "cloud.drizzle"
+            case 500...531:
+                return "cloud.rain"
+            case 600...622:
+                return "cloud.snow"
+            case 701...781:
+                return "cloud.fog"
+            case 800:
+                return "sun.max"
+            case 801...804:
+                return "cloud"
+            default: return "cloud"
+        }
+    }
+}
